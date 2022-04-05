c2cc3848f01d52d2d416605edeb3670a895481b4
Pronicio <52421988+Pronicio@users.noreply.github.com>
2022-04-05 20:03:07 +0200

    🚧 NFT-01 page

---
+++ src/views/NFT-01.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="NFT-01">
+
+    <nav>
+      <img class="bitcoin" src="../assets/NFT-01/images/svg/bitcoin.svg" alt="bitcoin-logo"/>
+      <div class="search-bar">
+        <img class="search-icon" src="../assets/NFT-01/images/svg/search.svg" alt="search-logo"/>
+        <input type="search" id="site-search" placeholder="Search related to NFT, Artist...">
+      </div>
+      <div class="navigation">
+        <a href="#">Discover</a>
+        <a href="#">About</a>
+        <a href="#">Artists</a>
+        <a href="#">Help</a>
+      </div>
+      <div class="connect-wallet-btn">
+        <p>Connect Wallet</p>
+      </div>
+    </nav>
+
+    <main>
+      <div class="home">
+        <div class="back"></div>
+        <div class="stars s1"></div>
+        <div class="stars s2"></div>
+        <h1>Next Biggest Expedition in the World<br>
+          of Cryptocurrency</h1>
+        <img class="background" src="../assets/NFT-01/images/background.png" alt="background"/>
+      </div>
+      <div class="treasures">
+        <h2>Rare and Super Unique<br>
+          NFT that you’ll love!</h2>
+        <div class="items">
+          <button id="before"></button>
+          <img src="../assets/NFT-01/images/items/NFT-base.png" alt="NFT-01"/>
+          <img src="../assets/NFT-01/images/items/NFT-exemple.png" alt="NFT-01"/>
+          <img src="../assets/NFT-01/images/items/NFT-base.png" alt="NFT-01"/>
+          <button id="after"></button>
+        </div>
+        <button class="discover-btn">Discover More Treasures</button>
+      </div>
+      <div class="travel">
+        <img src="../assets/NFT-01/images/travel.png" alt="Travel Image"/>
+        <div class="travel-right">
+          <h3>Travel With Us To<br>
+            A Land of Adventure</h3>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br> Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown</p>
+          <div class="social">
+            <button id="discord">Join Discord<i></i></button>
+            <button id="instagram"></button>
+            <button id="facebook"></button>
+          </div>
+        </div>
+      </div>
+      <div class="creators">
+        <h3>Meet Our Creators</h3>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+@import "../assets/NFT-01/style.css";
+</style>
