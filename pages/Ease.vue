7aa60e7fa1c46de78f5700c7c6e3bd662d429e66
Pronicio <52421988+Pronicio@users.noreply.github.com>
2023-01-19 18:47:18 +0100

    🚧 Ease Page.

---
+++ pages/Ease.vue
@@ -0,0 +1,23 @@
+<template>
+  <header></header>
+  <main></main>
+</template>
+
+<script>
+export default {
+  name: "Ease"
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/Ease/style.scss';
+</style>
+
+<style>
+
+body {
+  background: #060707;
+  font-family: 'IBM Plex Mono', sans-serif;
+  overflow: hidden;
+}
+</style>
